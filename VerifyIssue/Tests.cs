a3d3543570e560e81f36c24afd3b0cc873d11372
Simon Cropp <simon.cropp@gmail.com>
2025-04-15 20:22:53 +1000

    .

--- VerifyIssue/Tests.cs
+++ VerifyIssue/Tests.cs
@@ -1,43 +1,50 @@
 using System.Runtime.CompilerServices;
+using Argon;
 
 namespace VerifyIssue;
 
 public class Tests
 {
-    static Tests()=>
+    [ModuleInitializer]
+    public static void Initialize()
+    {
+        // https://github.com/VerifyTests/Verify/blob/main/docs/naming.md#derivepathinfo
+        DerivePathInfo(
+            (sourceFile, projectDirectory, type, method) => new(
+                directory: Path.Combine(projectDirectory, "Snapshots"),
+                // no type name is not supported in this API
+                typeName: type.Name,
+                methodName: method.Name));
+
+        // https://github.com/VerifyTests/Verify/blob/main/docs/serializer-settings.md#empty-collections-are-ignored
+        VerifierSettings.DontIgnoreEmptyCollections();
+
+        // https://github.com/VerifyTests/Verify/blob/main/docs/serializer-settings.md#usestrictjson
+        VerifierSettings.UseStrictJson();
+
+        // https://github.com/VerifyTests/Verify/blob/main/docs/serializer-settings.md#usestrictjson
+        VerifierSettings.IgnoreMember("traceId");
+
+        // https://github.com/VerifyTests/Verify/blob/main/docs/serializer-settings.md#modify-defaults
         VerifierSettings.AddExtraSettings(settings =>
         {
-            settings.DefaultValueHandling = Argon.DefaultValueHandling.Include;
-            settings.ContractResolver = new Argon.CamelCasePropertyNamesContractResolver();
-        });
-    
-    private VerifySettings GetVerifySettings(string methodName)
-    {
-        var settings = new VerifySettings();
-        settings.UseDirectory($"./Snapshots");
-        settings.UseFileName(methodName);
-        settings.UseStrictJson();
-        settings.DontIgnoreEmptyCollections();
-        settings.IgnoreMember("traceId"); // Emitted by ASP.NET model validation
-        return settings;
-    }
-    
-    public async Task VerifyJson(string? verifyJson, Action<VerifySettings>? verifySettings = null, [CallerMemberName] string methodName = "")
-    {
-        var settings = GetVerifySettings(methodName);
-        verifySettings?.Invoke(settings);
+            settings.DefaultValueHandling = DefaultValueHandling.Include;
 
-        await Verifier.VerifyJson(verifyJson, settings);
+            var resolver = (DefaultContractResolver) settings.ContractResolver!;
+            resolver.NamingStrategy = new CamelCaseNamingStrategy
+            {
+                ProcessDictionaryKeys = true,
+                OverrideSpecifiedNames = true
+            };
+        });
     }
-    
+
     [Fact]
     public async Task MyTest()
     {
         var target = "{\"id\": \"myId_1\",\"version\":{\"obsoleteDate\":null,\"resourceVersion\":\"2018-01-01\"},\"messages\":[],}";
 
-        await VerifyJson(target, verifySettings =>
-        {
-            verifySettings.IgnoreMember("id");
-        });
+        await VerifyJson(target)
+            .IgnoreMember("id");
     }
 }
